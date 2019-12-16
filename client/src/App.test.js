3fd66fffeb743b1960e45eb257198f9c77aed347
Anna Morris <annapatton444@gmail.com>
2019-12-15 21:48:57 -0600

    tests don't work

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,16 @@
-import React from "react";
-import * as rtl from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-afterEach(rtl.cleanup);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
-test("Check DarkMode", () => {
-  const wrapper = rtl.render(<App />);
-  const element = wrapper.queryByText(/dark/i);
-  expect(element).toBeVisible();
+test('darkmode', () => {
+  const { getByText } = rtl.render(<App />);
+  getByText(/dark/i)
 });
